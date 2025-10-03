11e8242371f8d8ead9ce33fd3d63c7dfb862f5c1
ginaa07 <1123150124@global.ac.id>
2025-10-03 09:42:33 +0700

    first season_3_practice

---
+++ bin/mobil.dart
@@ -0,0 +1,12 @@
+class Mobil {
+String merk;
+int tahun;
+Mobil(this.merk, this.tahun);
+void info() {
+print('Mobil: $merk, Tahun: $tahun');
+}
+}
+void main() {
+var innova = Mobil('Toyota', 2025);
+innova.info(); // Mobil: Toyota, Tahun: 2025
+}
